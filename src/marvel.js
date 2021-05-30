1e92b80b09bd0e1613d286d9971a56b1b9f514f4
vishrutisingh <70326494+vishrutisingh@users.noreply.github.com>
2021-05-30 15:23:51 +0530

    Initial commit

---
+++ src/marvel.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>LIST OF MARVEL MOVIES</h1>
+    <ul>
+      <li>Ironman</li>
+      <li>Thor : the dark world</li>
+      <li>Thor Ragnarok</li>
+      <li>Hulk</li>
+      <li>Captain America and the winter soldier </li>
+      <li>Captain America:The first Avenger </li>
+      <li>Antman</li>
+      <li>Avengers</li>
+      <li>Avengers : THE AGE OF ULTRON</li>
+      <li>Avengers : THE INFINITY WAR</li>
+      <li>Avengers : THE ENDGAME </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
